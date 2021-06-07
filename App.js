c89b986da22a99f75a7aee5b970afc3df7588aac
lgalant <lgalant@live.com>
2021-06-07 16:26:14 -0300

    update

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Principal from './paginas/principal'
+import Detalle from './paginas/detalle'
+import Settings from './paginas/settings'
 
+import {NavigationContainer} from '@react-navigation/native'
+
+import {createStackNavigator} from '@react-navigation/stack'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import {createDrawerNavigator} from '@react-navigation/drawer'
+
+
+//const miStack = createStackNavigator()
+const miBottomNav = createBottomTabNavigator()
+//const miStack = createDrawerNavigator()
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <miBottomNav.Navigator initialRouteName={"Pant-Principal"}>
+        <miBottomNav.Screen name="Pant-Principal" component={Principal}/>
+        <miBottomNav.Screen name="Configuracion" component={Settings} options={{title:'Config'}}/>
+      </miBottomNav.Navigator>
+    </NavigationContainer>
   );
 }
 
